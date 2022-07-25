a479ca7a3a49e3e7ad4c6160b8331f0f35f87c4e
Adil Shamshad <shamshad.adil@gmail.com>
2022-07-25 23:54:38 +0100

    Initial commit, creating barebones of this service. Building it a small Spring Boot Web API that will expose a convert endpoint. First test pushed that tests the Spring context.

---
+++ src/main/java/com/interview/integerromanconversionservice/IntegerRomanConversionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.interview.integerromanconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegerRomanConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegerRomanConversionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interview/integerromanconversionservice/IntegerRomanConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.integerromanconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegerRomanConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
