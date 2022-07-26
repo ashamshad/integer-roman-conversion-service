822b7dfc546e06e8d4f2ae999c2db9e5707ff494
Adil Shamshad <shamshad.adil@gmail.com>
2022-07-26 22:21:35 +0100

    Adding implementation for the roman to integer converter, tests failing because of NPE, so fixing it

--- src/main/java/com/interview/integerromanconversion/service/IntegerToRomanConverter.java
+++ src/main/java/com/interview/integerromanconversion/service/IntegerToRomanConverter.java
@@ -8,32 +8,32 @@ import java.util.TreeMap;
 @Service
 public class IntegerToRomanConverter implements Converter<Integer, String> {
 
-    private final static TreeMap<Integer, String> map = new TreeMap<>();
+    private final static TreeMap<Integer, String> INTEGER_ROMAN_MAP = new TreeMap<>();
     static {
-        map.put(1000, "M");
-        map.put(900, "CM");
-        map.put(500, "D");
-        map.put(400, "CD");
-        map.put(100, "C");
-        map.put(90, "XC");
-        map.put(50, "L");
-        map.put(40, "XL");
-        map.put(10, "X");
-        map.put(9, "IX");
-        map.put(5, "V");
-        map.put(4, "IV");
-        map.put(1, "I");
+        INTEGER_ROMAN_MAP.put(1000, "M");
+        INTEGER_ROMAN_MAP.put(900, "CM");
+        INTEGER_ROMAN_MAP.put(500, "D");
+        INTEGER_ROMAN_MAP.put(400, "CD");
+        INTEGER_ROMAN_MAP.put(100, "C");
+        INTEGER_ROMAN_MAP.put(90, "XC");
+        INTEGER_ROMAN_MAP.put(50, "L");
+        INTEGER_ROMAN_MAP.put(40, "XL");
+        INTEGER_ROMAN_MAP.put(10, "X");
+        INTEGER_ROMAN_MAP.put(9, "IX");
+        INTEGER_ROMAN_MAP.put(5, "V");
+        INTEGER_ROMAN_MAP.put(4, "IV");
+        INTEGER_ROMAN_MAP.put(1, "I");
     }
 
     public String convert(Integer number) {
         // Returns the greatest key less than or equal to the given key
-        int floor = map.floorKey(number);
+        int floor = INTEGER_ROMAN_MAP.floorKey(number);
         if (number == floor) {
             // We've reached the end of the number to convert, so we get out of the recursive loop and return the String
-            return map.get(number);
+            return INTEGER_ROMAN_MAP.get(number);
         }
 
         // There's still a leftover to convert, so recursively calling the function by subtracting the key found in map
-        return map.get(floor) + convert(number-floor);
+        return INTEGER_ROMAN_MAP.get(floor) + convert(number-floor);
     }
 }
--- src/main/java/com/interview/integerromanconversion/service/RomanToIntegerConverter.java
+++ src/main/java/com/interview/integerromanconversion/service/RomanToIntegerConverter.java
@@ -3,10 +3,41 @@ package com.interview.integerromanconversion.service;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class RomanToIntegerConverter implements Converter<String, Integer> {
 
+    private final static Map<Character,Integer> ROMAN_TO_INT_MAP = new HashMap<>();
+    static {
+        ROMAN_TO_INT_MAP.put('I', 1);
+        ROMAN_TO_INT_MAP.put('V', 5);
+        ROMAN_TO_INT_MAP.put('X', 10);
+        ROMAN_TO_INT_MAP.put('L', 50);
+        ROMAN_TO_INT_MAP.put('C', 100);
+        ROMAN_TO_INT_MAP.put('D', 500);
+        ROMAN_TO_INT_MAP.put('M', 1000);
+    }
+
     public Integer convert(String roman) {
-        return 0;
+        int result = 0;
+
+        for (int i = 0; i < roman.length(); i++) {
+
+            char currentRomanChar = roman.charAt(i);
+
+            if (i > 0) {
+                char previousRomanChar = roman.charAt(i);
+                // If the previous character is greater than the current one
+                if (ROMAN_TO_INT_MAP.get(currentRomanChar) > ROMAN_TO_INT_MAP.get(previousRomanChar)) {
+                    result += ROMAN_TO_INT_MAP.get(currentRomanChar) - 2 * ROMAN_TO_INT_MAP.get(previousRomanChar);
+                }
+            } else {
+                result += ROMAN_TO_INT_MAP.get(currentRomanChar);
+            }
+        }
+
+        return result;
     }
 }
