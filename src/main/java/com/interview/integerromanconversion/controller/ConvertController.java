20e4ac838ebc6fc63b1eb0302e9533f260500be8
Adil Shamshad <shamshad.adil@gmail.com>
2022-07-26 18:50:35 +0100

    Wiring the ConversionService into the Controller and failing the test

--- src/main/java/com/interview/integerromanconversion/controller/ConvertController.java
+++ src/main/java/com/interview/integerromanconversion/controller/ConvertController.java
@@ -1,15 +1,23 @@
 package com.interview.integerromanconversion.controller;
 
+import com.interview.integerromanconversion.service.ConversionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ConvertController {
+	private ConversionService conversionService;
+
+	@Autowired
+	public ConvertController(ConversionService conversionService) {
+		this.conversionService = conversionService;
+	}
 
 	@PostMapping("/convert/integer")
 	public String convertInteger(@RequestParam Integer integer) {
 		System.out.println(integer + " to be converted");
-		return "roman";
+		return this.conversionService.integerToRoman(integer);
 	}
 }
