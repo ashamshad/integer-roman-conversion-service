31db2252c2e72b5a0085572a8e816d718271d8bb
Adil Shamshad <shamshad.adil@gmail.com>
2022-07-26 18:53:47 +0100

    Injecting a mocked ConversionService into the test context, to test the controller in isolation

--- src/test/java/com/interview/integerromanconversion/controller/ConvertControllerTest.java
+++ src/test/java/com/interview/integerromanconversion/controller/ConvertControllerTest.java
@@ -1,14 +1,17 @@
 package com.interview.integerromanconversion.controller;
 
+import com.interview.integerromanconversion.service.ConversionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -19,10 +22,14 @@ public class ConvertControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private ConversionService conversionService;
+
     @Test
     public void convertInteger_nominalScenario_returnsConvertedInteger() throws Exception {
+        when(conversionService.integerToRoman(10)).thenReturn("converted");
         mvc.perform(MockMvcRequestBuilders.post("/convert/integer?integer=10").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("X")));
+                .andExpect(content().string(equalTo("converted")));
     }
 }
