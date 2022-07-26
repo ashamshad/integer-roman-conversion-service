4094987178f2a112ffebe9c6bf4cc89b45aba026
Adil Shamshad <shamshad.adil@gmail.com>
2022-07-26 18:20:10 +0100

    Adding data structure to support the conversion, and method implementation. Also failing the test.

--- src/main/java/com/interview/integerromanconversion/service/ConversionService.java
+++ src/main/java/com/interview/integerromanconversion/service/ConversionService.java
@@ -2,10 +2,33 @@ package com.interview.integerromanconversion.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.TreeMap;
+
 @Service
 public class ConversionService {
 
+    private final static TreeMap<Integer, String> map = new TreeMap<>();
+    static {
+        map.put(1000, "M");
+        map.put(900, "CM");
+        map.put(500, "D");
+        map.put(400, "CD");
+        map.put(100, "C");
+        map.put(90, "XC");
+        map.put(50, "L");
+        map.put(40, "XL");
+        map.put(10, "X");
+        map.put(9, "IX");
+        map.put(5, "V");
+        map.put(4, "IV");
+        map.put(1, "I");
+    }
+
     public String integerToRoman(int number) {
-        return "roman";
+        int l =  map.floorKey(number);
+        if ( number == l ) {
+            return map.get(number);
+        }
+        return map.get(l) + integerToRoman(number-l);
     }
 }
